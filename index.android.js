7df212499982208844b6b9b9dfc181e91133232e
hds <463141635@qq.com>
2016-04-14 18:56:14 +0800

    添加图片,页面跳转

--- index.android.js
+++ index.android.js
@@ -4,48 +4,89 @@
  */
 
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    Image,
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Navigator
 } from 'react-native';
+var HttpView = require('./http')
+// var Button = require('react-native-button');
 
 class HelloWorld extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    configureScenceAndroid() {
+        return Navigator.SceneConfigs.VerticalUpSwipeJump;
+    }
+    renderSceneAndroid(route, navigator) {
+        _navigator = navigator;
+        if (route.id === 'main') {
+            return (
+                <View>
+                    <Text style={styles.welcome }>
+                        this is god
+                    </Text>
+                    <Image source={require('./img/ic_launcher.png') }>
+                    </Image>
+                    <TouchableOpacity
+                        onPress={() => _navigator.push({ title: 'Http', id: 'http' }) }
+                        style={ styles.button}>
+                        <Text  style={styles.welcome}>
+                            Press Me!</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
+        if (route.id === 'http') {
+            return (
+                <HttpView navigator={navigator} route={route}/>
+            );
+        }
+    }
+
+    handlePress() {
+        console.log('what the hell')
+    }
+    render() {
+        var configureScence = this.configureScenceAndroid;
+        var renderScene = this.renderSceneAndroid;
+        return (
+            <Navigator
+                debugOverlay={false}
+                initialRoute={{ title: 'Main', id: 'main' }}
+                renderScene={renderScene}
+                />
+        );
+    }
+    handlePress() {
+        console.log('god,you');
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
